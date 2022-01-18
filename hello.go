f5bd83e5ef1cdf937967af8a9afcceeaad21b32b
anu1097 <anuraagg.kval3@gmail.com>
2022-01-19 01:33:10 +0530

    feat: second commit

--- hello.go
+++ hello.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 
-    "github.com/anu1097/golang_module/greetings_2"
-    "github.com/anu1097/golang_module/greetings_1"
+	"github.com/anu1097/golang_module/greetings_1"
+	"github.com/anu1097/golang_module/greetings_2"
 )
 
 func greeting_1() {
-    message := greetings_1.Hello("Gladys")
-    fmt.Println(message)
+	message := greetings_1.Hello("Gladys")
+	fmt.Println(message)
 }
 
 func greeting_2() {
-    message := greetings_2.Hello("Gladys")
-    fmt.Println(message)
+	message := greetings_2.Hello("Gladys")
+	fmt.Println(message)
 }
 
 func main() {
-    // Get a greeting message and print it.
-    greeting_1()
+	// Get a greeting message and print it.
+	greeting_1()
+	greeting_2()
 }
-
